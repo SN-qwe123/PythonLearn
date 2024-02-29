29ef6cd2275b6b090a752bb50eab8c16cd96c268
JustinNiu <buptnsy@gmail.com>
2024-02-28 22:55:11 -0500

    dict

--- RefreshWithUdemy.py
+++ RefreshWithUdemy.py
@@ -1,3 +1,34 @@
+
+#dictionary
+friend_ages = {
+  "Rolf":24,
+  "Adam": 30,
+  "Anne": 27
+}
+
+print(friend_ages["Adam"])
+friend_ages["Anne"] = 21
+print(friend_ages)
+
+'''
+#using for loop to calculate average number of a list
+grades = [35, 67, 88, 93, 100, 45]
+total = 0
+amount = len(grades)
+
+for grade in grades:
+  total += grade
+  #print(total)
+
+#total = sum(grades)
+average_grade = total/amount
+print(f"The average score is {average_grade}")
+
+'''
+
+
+'''
+#while loop
 number = 7
 
 while True:
@@ -12,4 +43,4 @@ while True:
     print("You were off by 1.")
   else:
     print("Sorry, it's wrong!")
-
+'''
